52d0ad63d2764e0039f4d2d354e96cdcaab92e06
divyesh sarvaiya <divyeshsarvaiya999@gmail.com>
2024-02-23 18:13:02 +0530

    add a backdrop filter in this commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,14 @@ const navMenu = document.getElementById('nav-menu'),
       }
 
       navLink.forEach(n => n.addEventListener('click',linkAction))
+
+
+
+      const blurHeader = () =>{
+            const header = document.getElementById('header')
+
+            this.scrollY >=50 ? header.classList.add('blur-header')
+                              : header.classList.remove('blur-header')
+      }
+
+      window.addEventListener('scroll',blurHeader)
