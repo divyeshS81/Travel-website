2bc51f7e398135143e33e51baa64e4e42cfdc7fa
divyesh sarvaiya <divyeshsarvaiya999@gmail.com>
2024-03-07 11:26:01 +0530

    add a scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,13 @@ const navMenu = document.getElementById('nav-menu'),
       }
 
       window.addEventListener('scroll',blurHeader)
+
+
+      const scrollup = () =>{
+            const scrollup = document.getElementById('scroll-up')
+
+            this.scrollY >= 350 ? scrollup.classList.add('show-scroll')
+                                : scrollup.classList.remove('show-scroll') 
+      }
+
+      window.addEventListener('scroll',scrollup)
